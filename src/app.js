35af97e99391e7956cd1b787eab3f65ffd55623e
sarahkate87 <86667923+sarahkate87@users.noreply.github.com>
2021-07-11 16:36:04 +0100

    Formatted date and time to display live data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,47 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let day = days[date.getDay()];
+  let dayNum = date.getDate();
+  let month = months[date.getMonth()];
+  return `${day} ${dayNum} ${month}`;
+}
+
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#country").innerHTML = response.data.sys.country;
@@ -6,6 +50,12 @@ function displayTemperature(response) {
   );
   document.querySelector("#condition").innerHTML =
     response.data.weather[0].main;
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+  document.querySelector("#time").innerHTML = formatTime(
+    response.data.dt * 1000
+  );
 }
 
 let apiKey = "9e5720bf1c8d5cf0a9989c3fb45bc7a2";
